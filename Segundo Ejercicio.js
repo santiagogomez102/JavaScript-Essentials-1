7af7f1f275e83121e54bfa933fa1fc966183a531
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 17:13:11 -0500

    2.1.3 Boolean

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,11 +1,11 @@
-let year = 1990;
-console.log(typeof year);
-console.log(typeof 1991);
+"use strict"
 
-let name = "Alice";
-console.log(typeof name);
-console.log(typeof "Bob");
+let isDataValid = true;
+let isStringTooLong = false;
+let isGameOver = false;
+let continueLoop = true;
 
-let typeOfYear = typeof year;
-console.log(typeOfYear);
-console.log(typeof typeOfYear);
+console.log(false);
+console.log(typeof false);
+console.log(isDataValid);
+console.log(typeof isDataValid);
