2c0587cf46a1761cc8c98617b110afa929cc67b8
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 13:54:43 -0500

    2.0.12
    Hoisting

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,12 +1,5 @@
 "use strict"
 
-var counter = 100;
-
-function testFunction() {
-    var counter = 200;
-    console.log(counter);
-}
-
-console.log(counter);
-testFunction();
-console.log(counter);
+var height = 180;
+console.log(height);
+console.log(weight);
