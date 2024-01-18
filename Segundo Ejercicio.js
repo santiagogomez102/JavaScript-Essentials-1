678332f01096c919c462c41138f3d1b77fbc6cc8
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 12:39:58 -0500

    2.0.8 with nested blocks
    Global and local variables depends on the level of the block

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,10 +1,15 @@
 "use strict"
 
-let height = 180;
+let height = 200;
 {
-    let weight = 70;
+    let weight = 100;
+    {
+        let info = "tall";
+        console.log(height);
+        console.log(weight);
+        console.log(info);
+    }
     console.log(height);
     console.log(weight);
+    console.log(info);
 }
-console.log(height);
-console.log(weight);
