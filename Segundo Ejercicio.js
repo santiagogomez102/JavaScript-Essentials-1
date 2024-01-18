3b58c509b0ec7a7e152b3ded3acf58333aaa6e0e
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 11:05:04 -0500

    2.0.6 change data type
    Java allows variables to change data type

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -5,3 +5,9 @@ steps = 120;
 console.log(steps);
 steps = steps + 200;
 console.log(steps);
+
+let greeting = "Hello!";
+let counter = 100;
+console.log(greeting);
+greeting = 1;
+console.log(greeting);
