9a4a02a7d3a514b6448ca30e69487f3441c269d6
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 20:11:35 -0500

    2.1.10
    Implicit Conversions

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,16 +1,9 @@
 "use strict"
 
-console.log(BigInt(11));
-console.log(BigInt(0x11));
-console.log(BigInt(11e2));
+const str1 = 42 + "1";
+console.log(str1);
+console.log(typeof str1);
 
-console.log(BigInt(true));
-
-console.log(BigInt("11"));
-console.log(BigInt("0x11"));
-
-//console.log(BigInt(null));        Converting from "null" doesn't work
-
-//console.log(BigInt(undefined));   Converting from "undefined" doesn't work
-
-//console.log(BigInt(NaN));         Converting from "NaN" doesn't work
+const str2 = 42 - "1";
+console.log(str2);
+console.log(typeof str2);
