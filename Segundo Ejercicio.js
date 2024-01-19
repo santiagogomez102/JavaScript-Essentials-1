39b8199a3281915bbb2825b374f418d8f8208aac
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 19:17:45 -0500

    2.1.8
    Conversion to Boolean

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,21 +1,14 @@
 "use strict"
 
-console.log(Number(42));
+console.log(Boolean(true));
 
-console.log(Number("11"));
-console.log(Number("0x11"));
-console.log(Number("0o11"));
-console.log(Number("0b11"));
-console.log(Number("12e3"));
-console.log(Number("Infinity"));
-console.log(Number("text"));
+console.log(Boolean(42));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
 
-console.log(Number(14n));
-console.log(Number(123456789123456789123n));
+console.log(Boolean("text"));
+console.log(Boolean(""));
 
-console.log(Number(true));
-console.log(Number(false));
+console.log(Boolean(undefined));
 
-console.log(Number(undefined));
-
-console.log(Number(null));
+console.log(Boolean(null));
