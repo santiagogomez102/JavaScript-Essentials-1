70e8a70b5039dd4d99b71fd50937dc2588df38ac
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-18 19:07:02 -0500

    2.1.6
    Conversion to String

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,14 +1,31 @@
 "use strict"
 
-const num = 42;
+let str = "text";
+let strStr = String(str);
+console.log(`${typeof str} : ${str}`);
+console.log(`${typeof strStr} : ${strStr}`);
 
-const strFromNum1 = String(num);    //"42"
-console.log(strFromNum1);
-const strFromNum2 = String(8);      //"8"
-console.log(strFromNum2);
-const strFromBool = String(true);   //"true"
-console.log(strFromBool);
-const numFromStr = Number("312");   //312
-console.log(numFromStr);
-const boolFromNumber = Boolean(0);  //false
-console.log(boolFromNumber);
+let nr = 42;
+let strNr = String(nr);
+console.log(`${typeof nr} : ${nr}`);
+console.log(`${typeof strNr} : ${strNr}`);
+
+let bl = true;
+let strBl = String(bl);
+console.log(`${typeof bl} : ${bl}`);
+console.log(`${typeof strBl} : ${strBl}`);
+
+let bnr = 123n;
+let strBnr = String(bnr);
+console.log(`${typeof bnr} : ${bnr}`);
+console.log(`${typeof strBnr} : ${strBnr}`);
+
+let un = undefined;
+let strUn = String(un);
+console.log(`${typeof un} : ${un}`);
+console.log(`${typeof strUn} : ${strUn}`);
+
+let n = null;
+let strN = String(n);
+console.log(`${typeof n} : ${n}`);
+console.log(`${typeof strN} : ${strN}`);
