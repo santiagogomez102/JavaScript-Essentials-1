aa2a4ddef080dcab827f70522e4d56ae684b73d8
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 13:39:03 -0500

    2.2.3 Practice

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,74 +1,60 @@
 "use strict"
 
-let names = ["Olivia", "Emma", "Mateo", "Samuel"];
+let ticket = {
+    from: "Bogota",
+    to: "Chia",
+    price: 10000
+};
 
-//.length returns the length of the array
+console.log(`Ticket from: ${ticket.from}`);
+console.log(`Ticket to: ${ticket.to}`);
+console.log(`Ticket price: ${ticket.price}`);
 
-console.log(names.length);
+let person = {};
 
-names[5] = "Amelia";
-console.log(names.length);
+person.name = "Santiago";
+person.surname = "Gomez";
 
-console.log(names);
-console.log(names[3]);
-console.log(names[4]);
-console.log(names[5]);
+console.log(`${person.name} ${person.surname}`);
 
-//.indexOf returns the position of the element
+let books = [
+    {
+        title: "Speaking JavaScript",
+        author: "Axel Rauschmayer",
+        pages: 460
+    },
+    {
+        title: "Programming JavaScript Applications",
+        author: "Eric Elliot",
+        pages: 254
+    },
+    {
+        title: "Understanding ECMAScript 6",
+        author: "Nicholas C. Zakas",
+        pages: 352
+    }
+];
 
-console.log(names.indexOf("Mateo"));
-console.log(names.indexOf("Victor"));
+books.push({
+    title: "Learning JavaScript Design Patterns",
+    author: "Addy Osmani",
+    pages: 254
+});
 
-//.push places the element to the end of the array
+console.log(books.length);
+console.log(books[0].title);
+console.log(books[1].title);
+console.log(books[2].title);
+console.log(books[3].title);
 
-names.push("Santiago");
-console.log(names.length);
-console.log(names);
+console.log(books.slice(-2));
 
-//.unshift places the element at the beginning of the array
+books.shift();
 
-names.unshift("Paula");
-console.log(names.length);
-console.log(names);
+console.log(books.length);
+console.log(books[0].title);
+console.log(books[1].title);
+console.log(books[2].title);
 
-//.pop returns the last element of the array and deletes it from the array
-
-let name = names.pop();
-console.log(names.length);
-console.log(name);
-console.log(names);
-
-//.shift returns the first element of the array and deletes it from the array
-
-let name1 = names.shift();
-console.log(names.length);
-console.log(name1);
-console.log(names);
-
-//.reverse reverses the arrays, making the first element the last, the second will be the last but one, ...
-
-names.reverse();
-console.log(names);
-
-//.slice creates a new array from selected elements of the original array
-
-let n1 = names.slice(2);        //Copies from position 2 to end
-console.log(n1);
-
-let n2 = names.slice(1,3);      //Copies from position 1 to 3
-console.log(n2);
-
-let n3 = names.slice(0,-1);     //Copies from position 0 to the end -1
-console.log(n3);
-
-let n4 = names.slice(-1);       //Copies from end to end -1
-console.log(n4);
-
-//.concat creates a new array attaching elements from an array to the original
-
-let otherNames = ["William", "Jane"];
-let allNames = names.concat(otherNames);
-
-console.log(names);
-console.log(otherNames);
-console.log(allNames);
+let totalPages = books[0].pages + books[0].pages + books[0].pages;
+console.log(`Pages: ${totalPages}`);
