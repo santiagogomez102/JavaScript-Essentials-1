8e7aa1f819748b3322f739d2e83c3ef1a8b1de2b
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 14:04:39 -0500

    2.2.5
    LAB

--- LAB.js
+++ LAB.js
@@ -1,11 +1,29 @@
-let name1 = "Maxwell Wright";
-let name2 = "Raja Villarreal";
-let name3 = "Helen Richards";
-let phone1 = "(0191) 719 6495";
-let phone2 = "0866 398 2895";
-let phone3 = "0800 1111";
-let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
-let email2 = "posuere.vulputate@sed.com";
-let email3 = "libero@convallis.edu";
-console.log(name1 + " / " + phone1 + " / " + email1);
-console.log(name3 + " / " + phone3 + " / " + email3);
+let contacts = [
+    {
+        name: "Maxwell Wright",
+        phone: "(0191) 719 6495",
+        email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    },
+    {
+        name: "Raja Villarreal",
+        phone: "0866 398 2895",
+        email: "posuere.vulputate@sed.com"
+    },
+    {
+        name: "Helen Richards",
+        phone: "0800 1111",
+        email: "libero@convallis.edu"
+    }
+];
+
+let newContact = {
+    name: "Maisie Haley",
+    phone: "0913 531 3030",
+    email: "risus.Quisque@urna.ca"
+}
+
+contacts.push(newContact);
+
+length = contacts.length - 1;
+console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
+console.log(`${contacts[length].name} / ${contacts[length].phone} / ${contacts[length].email}`);
