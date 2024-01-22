02beb164e1ab103e5beac98f7d8b513191a223c1
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 09:22:23 -0500

    2.2.2 Identity arrays
    The "typeof" operator returns "object when using it on an array, so the "instanceof" operator should be used

--- Segundo Ejercicio.js
+++ Segundo Ejercicio.js
@@ -1,27 +1,10 @@
 "use strict"
 
-let users = [
-    {
-        name: "Calvin",
-        surname: "Hart",
-        age: 66,
-        email: "CalvinMHart@teleworm.us"
-    },
-    {
-        name: "Mateus",
-        surname: "Pinto",
-        age: 21,
-        email: "MateusPinto@dayrep.com"
-    }
-];
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", ];
+let day = "Sunday";
 
-users[2] = {
-    name: "Irene",
-    surname: "Purnell",
-    age: 32,
-    email: "IreneHPurnell@rhyta.com"
-}
+console.log(typeof days);
+console.log(typeof day);
 
-console.log(users[0].name);
-console.log(users[1].name);
-console.log(users[2].name);
+console.log(days instanceof Array);
+console.log(day instanceof Array);
