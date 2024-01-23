3535ed17cce152460f5ccb89f365c7743840e19c
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-23 18:20:57 -0500

    4.0.3
    if ... else

--- Cuarto Modulo.js
+++ Cuarto Modulo.js
@@ -1,38 +1,8 @@
 "use strict"
 
 let isUserReady = confirm("Are you ready?");
-console.log(isUserReady);
-if (isUserReady)            //if can be used without brackets, but only the next line belongs to it
-    alert("User ready!");
-
-isUserReady = confirm("Are you ready?");
-if (isUserReady) {
+if (isUserReady){
     console.log("User ready!");
-    alert("User ready!");
-}
-
-let unitPrice = 10;
-let pieces = prompt("How many pieces do you order?", 0);
-if (pieces > 0) {
-    let total = unitPrice * pieces;
-    console.log(total);
-}
-// console.log(total);      "total" variable was declared inside the "if", therefore it doesn't exist outside
-
-let userAge = 23;
-let isFemale = false;
-let points = 703;
-let cartValue = 299;
-let shippingCost = 9.99;
-
-if (userAge > 21){
-    if(cartValue >= 300 || points >= 500){
-        shippingCost = 0;
-    }
-}
-
-console.log(shippingCost);
-
-if (userAge > 21 && (cartValue >= 300 || points >= 500)){   //  Using && one line gets deleted
-    shippingCost = 0;
+} else {
+    console.log("User not ready!");
 }
