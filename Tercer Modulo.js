a22020db883285f64abbda3ab427de59c71908aa
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 22:22:12 -0500

    3.1.2
    Comparison operators

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,9 +1,60 @@
 "use strict"
 
-let greetings = "Hi";
-console.log(greetings + " " + "Alice");
-let sentence = "Happy New ";
-sentence += "Year ";
-let newSentence =  sentence + 10191;
-console.log(newSentence);
-console.log(typeof newSentence);
+console.log(10 === 5);  // More strict
+console.log(10 === 10);
+console.log(10 === 10n);
+console.log(10 === "10");
+console.log("10" === "10");
+console.log("Alice" === "Bob");
+console.log(0 === false);
+console.log(undefined === false);
+
+console.log(10 == 5);
+console.log(10 == 10);
+console.log(10 == 10n);
+console.log(10 == "10");
+console.log("10" == "10");
+console.log("Alice" == "Bob");
+console.log(0 == false);
+console.log(undefined == false);
+console.log(NaN == NaN);
+
+console.log(10 !== 5);  // More strict
+console.log(10 !== 10);
+console.log(10 !== 10n);
+console.log(10 !== "10");
+console.log("10" !== "10");
+console.log("Alice" !== "Bob");
+console.log(0 !== false);
+console.log(undefined !== false);
+
+console.log(10 != 5);
+console.log(10 != 10);
+console.log(10 != 10n);
+console.log(10 != "10");
+console.log("10" != "10");
+console.log("Alice" != "Bob");
+console.log(0 != false);
+console.log(undefined != false);
+console.log(NaN != NaN);
+
+console.log(10 > 100);
+console.log(101 > 100);
+console.log(101 > "100");
+console.log(101 < 100);
+console.log(100n < 102);
+console.log("10" < 20n);
+console.log(101 <= 100);
+console.log(10 >= 10n);
+console.log("10" <= 20);
+
+console.log("b" > "a");
+console.log("a" > "B");
+console.log("B" > "A");
+console.log("A" > "4");
+console.log("4" > "1");
+console.log("ab1" < "ab4");
+console.log("ab4" < "abA");
+console.log("abB" < "aba");
+console.log("aba" < "abb");
+console.log("ab" < "ab4");
