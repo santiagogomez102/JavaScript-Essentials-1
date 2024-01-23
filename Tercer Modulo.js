241140cdc6f4728c873730923927a8b3f7237d75
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-23 12:50:05 -0500

    3.2.2 Alert dialog box

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,14 +1,7 @@
 "use strict"
 
-let a = 10;
-let b = a + 2 * 3;
-let c = a + 2 < 20 - 15;
-console.log(a);
-console.log(b);
-console.log(c);
-
-let d, e;
-e = (d = (20 + 20) * 2) > (3 ** 2);
-
-console.log(d);
-console.log(e);
+alert("Hello, World!");
+window.alert("Hello, World! for the second time");
+alert(4 * 7);
+alert(true);
+alert("text 1", "text 2");
