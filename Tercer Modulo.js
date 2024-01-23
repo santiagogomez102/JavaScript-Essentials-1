9b617674dc24df9d5f797463f8293451a3b4c6f4
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 20:55:56 -0500

    3.0.6
    Compound Assignment

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,36 +1,16 @@
 "use strict"
 
-let n1 = 10;
-let n2 = 10;
+let x = 10;
 
-console.log(n1);
-console.log(n1++);
-console.log(n1);
-
-console.log(n2);
-console.log(++n2);
-console.log(n2);
-
-let n3 = 20;
-let n4 = 20;
-
-console.log(n3);
-console.log(n3--);
-console.log(n3);
-
-console.log(n4);
-console.log(--n4);
-console.log(n4);
-
-/*
-console.log(n1++) is the same as:
-console.log(n1);
-n1 = n1 + 1;
-while console.log(++n1) is the same as:
-n1 = n1 + 1;
-console.log(n1);
-*/
-
-console.log(0.2 + 0.1); // 0.30000000000000004 All these values are not precise as the number has to be divisable by 252
-console.log(0.2 * 0.1); // 0.020000000000000004 All these values are not precise as the number has to be divisable by 252
-console.log(0.3 / 0.1); // 2.9999999999999996 All these values are not precise as the number has to be divisable by 252
+x += 2;   //Same as x = x + 2;
+console.log(x);
+x -= 4;   //Same as x = x - 4;
+console.log(x);
+x *= 3;   //Same as x = x * 3;
+console.log(x);
+x /= 6;   //Same as x = x / 6;
+console.log(x);
+x **= 3;   //Same as x = x ** 3;
+console.log(x);
+x %= 10;   //Same as x = x % 10;
+console.log(x);
