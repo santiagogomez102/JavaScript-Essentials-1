5586a7bad1af1692aaca32f0504b3da01a53a1ab
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 21:55:24 -0500

    3.0.10
    Practice

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,11 +1,17 @@
 "use strict"
 
-let a = true;
-console.log(a);
-a &&= false;    // Same as a = a && false;
-console.log(a);
+console.log(2 * 3 + 1);
+console.log(2 ** 4);
+console.log(5 / 1);
+console.log(8 ** 2 - 5 ** 2)
 
-let b = false;
-console.log(b);
-b ||= true;     // Same as b = b || true;
-console.log(b);
+console.log(4 * 5 == 20);
+console.log(6 * 5 == "30");
+console.log(-17 < 0);
+console.log(25 > 1);
+console.log(2 + 2 * 2 != 4);
+
+console.log(true || false);
+console.log(false || !false);
+console.log(false || false || true);
+console.log(true || false && false && true);
