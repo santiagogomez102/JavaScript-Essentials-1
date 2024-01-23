4a45db1750e092ad191a38895a3edcacd62327c9
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-23 12:54:32 -0500

    3.2.2 Confirm dialog box

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,7 +1,5 @@
 "use strict"
 
-alert("Hello, World!");
-window.alert("Hello, World! for the second time");
-alert(4 * 7);
-alert(true);
-alert("text 1", "text 2");
+let remove = confirm("Remove all data?");
+let message = remove ? "Deleting Data" : "Cancelled";
+console.log(message);
