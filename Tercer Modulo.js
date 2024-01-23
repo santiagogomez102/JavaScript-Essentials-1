9d0afec0dc7b0e87a07dcdafc2bdda962d368279
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 20:13:36 -0500

    3.0.3
    Arithmetic operators

---
+++ Tercer Modulo.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let year = 2050;
+let newYear = year = 2051;
+
+/* Previous code is the same as:
+let year = 2050;
+year = 2051;
+let newYear = year;
+*/
+
+const x = 5;
+const y = 2;
+console.log(`Addition: `, x + y);
+console.log(`Subtraction: `, x - y);
+console.log(`Multiplication: `, x * y);
+console.log(`Division: `, x / y);
+console.log(`Division reminder: `, x % y);
+console.log(`Exponent: `, x ** y);
