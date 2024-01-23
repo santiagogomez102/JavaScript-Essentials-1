fdec6fcd90051d6cf0075832b5aeacb59e263c89
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 22:29:17 -0500

    3.1.3
    Ternary

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,60 +1,7 @@
 "use strict"
 
-console.log(10 === 5);  // More strict
-console.log(10 === 10);
-console.log(10 === 10n);
-console.log(10 === "10");
-console.log("10" === "10");
-console.log("Alice" === "Bob");
-console.log(0 === false);
-console.log(undefined === false);
+console.log(true ? "Alice" : "Bob");
+console.log(false ? "Alice" : "Bob");
 
-console.log(10 == 5);
-console.log(10 == 10);
-console.log(10 == 10n);
-console.log(10 == "10");
-console.log("10" == "10");
-console.log("Alice" == "Bob");
-console.log(0 == false);
-console.log(undefined == false);
-console.log(NaN == NaN);
-
-console.log(10 !== 5);  // More strict
-console.log(10 !== 10);
-console.log(10 !== 10n);
-console.log(10 !== "10");
-console.log("10" !== "10");
-console.log("Alice" !== "Bob");
-console.log(0 !== false);
-console.log(undefined !== false);
-
-console.log(10 != 5);
-console.log(10 != 10);
-console.log(10 != 10n);
-console.log(10 != "10");
-console.log("10" != "10");
-console.log("Alice" != "Bob");
-console.log(0 != false);
-console.log(undefined != false);
-console.log(NaN != NaN);
-
-console.log(10 > 100);
-console.log(101 > 100);
-console.log(101 > "100");
-console.log(101 < 100);
-console.log(100n < 102);
-console.log("10" < 20n);
-console.log(101 <= 100);
-console.log(10 >= 10n);
-console.log("10" <= 20);
-
-console.log("b" > "a");
-console.log("a" > "B");
-console.log("B" > "A");
-console.log("A" > "4");
-console.log("4" > "1");
-console.log("ab1" < "ab4");
-console.log("ab4" < "abA");
-console.log("abB" < "aba");
-console.log("aba" < "abb");
-console.log("ab" < "ab4");
+let name = 1 > 2 ? "Alice" : "Bob";
+console.log(name);
