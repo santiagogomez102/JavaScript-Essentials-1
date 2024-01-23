f55b3ee569d323166b01b7b3b67f242670386ffc
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 20:43:38 -0500

    3.0.5
    Unary increment and decrement

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,12 +1,36 @@
 "use strict"
 
-let str = "123";
-let n1 = +str;
-let n2 = -str;
-let n3 = -n2;
-let n4 = +"abdc";
-console.log(`${str} : ${typeof str}`);
-console.log(`${n1} : ${typeof n1}`);
-console.log(`${n2} : ${typeof n2}`);
-console.log(`${n3} : ${typeof n3}`);
-console.log(`${n4} : ${typeof n4}`);
+let n1 = 10;
+let n2 = 10;
+
+console.log(n1);
+console.log(n1++);
+console.log(n1);
+
+console.log(n2);
+console.log(++n2);
+console.log(n2);
+
+let n3 = 20;
+let n4 = 20;
+
+console.log(n3);
+console.log(n3--);
+console.log(n3);
+
+console.log(n4);
+console.log(--n4);
+console.log(n4);
+
+/*
+console.log(n1++) is the same as:
+console.log(n1);
+n1 = n1 + 1;
+while console.log(++n1) is the same as:
+n1 = n1 + 1;
+console.log(n1);
+*/
+
+console.log(0.2 + 0.1); // 0.30000000000000004 All these values are not precise as the number has to be divisable by 252
+console.log(0.2 * 0.1); // 0.020000000000000004 All these values are not precise as the number has to be divisable by 252
+console.log(0.3 / 0.1); // 2.9999999999999996 All these values are not precise as the number has to be divisable by 252
