51a17b106936deeae70d7ec5bf456412ac7050f3
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-22 21:32:56 -0500

    3.0.9
    Compound Assignment Operators

--- Tercer Modulo.js
+++ Tercer Modulo.js
@@ -1,31 +1,11 @@
 "use strict"
 
-let nr = 0;
-let year = 1970;
-let name = "Alice";
-let empty = "";
-console.log(!nr);
-console.log(!year);
-console.log(!name);
-console.log(!empty);
-console.log(!!nr);
-console.log(!!name);
+let a = true;
+console.log(a);
+a &&= false;    // Same as a = a && false;
+console.log(a);
 
-console.log(true && 1991);
-console.log(false && 1991);
-console.log(2 && 5);
-console.log(0 && 5);
-console.log("Alice" && "Bob");
-console.log("" && "Bob");
-console.log(true || 1991);
-console.log(false || 1991);
-console.log(2 || 5);
-console.log(0 || 5);
-console.log("Alice" || "Bob");
-console.log("" || "Bob");
-
-let x = 0;
-let y = 0;
-console.log(x++ && y++);
-console.log(x);
-console.log(y);
+let b = false;
+console.log(b);
+b ||= true;     // Same as b = b || true;
+console.log(b);
