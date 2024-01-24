da2b898d62b3fe6e31b19161debd612b4fae2978
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-24 16:25:17 -0500

    4.1.8
    break and continue

--- Cuarto Modulo.js
+++ Cuarto Modulo.js
@@ -1,14 +1,18 @@
 "use strict"
 
-let user = {
-    name: "Calvin",
-    surname: "Hart",
-    age: 66,
-    email: "CalvinMHart@teleworm.us"
-};
+let i = 0;
+while (true) {
+    console.log(i);
+    i++;
+    if (i >= 5){
+        break;
+    }
+}
+alert(`Exited the loop with a break (${i})`);
 
-console.log(user.name);
-
-for (let key in user){
-    console.log(`${key} -> ${user[key]}`);
+for (let i = 0; i < 10; i++) {
+    if (i == 3) {
+        continue;
+    }
+    console.log(i);
 }
