c8058075a18887f1f4981496aab4308881d9403d
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-23 20:45:04 -0500

    4.0.9
    LAB

--- LAB.js
+++ LAB.js
@@ -18,26 +18,31 @@ let contacts = [
     }
 ];
 
-let newContact = {
-    name: "Maisie Haley",
-    phone: "0913 531 3030",
-    email: "risus.Quisque@urna.ca"
-}
-
-contacts.push(newContact);
-
-let newName = prompt("Input the new contact name:", "Name and surname");
-let newPhone = prompt("Input the new phone number:", "Phone number");
-let newEmail = prompt("Input the new email address:", "Email address");
-
-newContact = {
-    name: newName,
-    phone: newPhone,
-    email: newEmail
-}
-
-contacts.push(newContact);
-
 let last = contacts.length - 1;
-console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
-console.log(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+
+let option = prompt("Select an action: 1. Show first contact 2. Show last contact 3. Add new concact", "1, 2, 3");
+switch (option) {
+    case "1": alert(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`); break;
+    case "2": alert(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`); break;
+    case "3":
+        let newName = prompt("Input the new contact name:", "Name and surname");
+        newName = newName ? newName : "NaN";
+        let newPhone = prompt("Input the new phone number:", "Phone number");
+        newPhone = newPhone ? newPhone : "NaN";
+        let newEmail = prompt("Input the new email address:", "Email address");
+        newEmail = newEmail ? newEmail : "NaN";
+        if (newName == "NaN" || newPhone == "NaN" || newEmail == "NaN"){
+            alert("Contact not added");
+        } else {
+            let newContact = {
+                name: newName,
+                phone: newPhone,
+                email: newEmail
+            }
+            contacts.push(newContact);
+            last = contacts.length - 1;
+            alert(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
+        }
+        break;
+    default: alert("Error with option input");
+}
