c09624d3534ae8d1085c299c70b530a2602d435f
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-24 15:03:05 -0500

    4.1.2
    while

--- Cuarto Modulo.js
+++ Cuarto Modulo.js
@@ -1,36 +1,13 @@
 "use strict"
 
-let numberInput = prompt("Input a number here!", "Number");
-if (numberInput > 90 && numberInput < 110){
-    alert("Bingo!");
-} else {
-    alert("Miss!");
-};
-
-let number = prompt("Input a number here!", "Number");
-let message = (number > 90 && number < 110) ? "Bingo!" : "Miss!";
-alert(message);
-
-alert("Input 2 numbers:");
-
-let n1 = Number(prompt("Number 1", 0));
-let n2 = Number(prompt("Number 2", 0));
-
-alert("Input a character from the following options: +, -, *, /");
-
-let s1 = prompt("Symbol 1", "+, -, *, /");
-
-let t1;
-
-if((!Number.isNaN(n1)) && (!Number.isNaN(n2))){
-    switch (s1) {
-        case "+": t1 = n1 + n2; break;
-        case "-": t1 = n1 - n2; break;
-        case "*": t1 = n1 * n2; break;
-        case "/": t1 = n1 / n2; break;
-        default: t1 = "An error has ocurred with the symbols";
-    }
-} else {
-    alert("An error has ocurred with the numbers");
+let n = 0;
+while(n < 91) {
+    console.log(n); // -> 0, 10, 20, 30, 40, 50, 60, 70, 80, 90
+    n += 10;
 }
-alert(t1)
+
+let isOver = false;
+let counter = 1;
+while (!isOver) {
+    isOver = !confirm(`[${counter++}] Continue the loop?`);
+}
