1e14be46e98c0ff8fea124e6e4e020b7d57f66a6
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-23 20:12:33 -0500

    4.0.8
    Practice

--- Cuarto Modulo.js
+++ Cuarto Modulo.js
@@ -1,21 +1,36 @@
 "use strict"
 
-let gate = prompt("Choose gate: a, b, c");
-let win = false;
-switch (gate) {
-    case "a":
-        alert("Gate A: empty");
-        break;
-    case "b":
-        alert("Gate B: main prize");
-        win = true;
-        break;
-    case "c":
-        alert("Gate C: empty");
-        break;
-    default:
-        alert("No gate " + String(gate));
+let numberInput = prompt("Input a number here!", "Number");
+if (numberInput > 90 && numberInput < 110){
+    alert("Bingo!");
+} else {
+    alert("Miss!");
+};
+
+let number = prompt("Input a number here!", "Number");
+let message = (number > 90 && number < 110) ? "Bingo!" : "Miss!";
+alert(message);
+
+alert("Input 2 numbers:");
+
+let n1 = Number(prompt("Number 1", 0));
+let n2 = Number(prompt("Number 2", 0));
+
+alert("Input a character from the following options: +, -, *, /");
+
+let s1 = prompt("Symbol 1", "+, -, *, /");
+
+let t1;
+
+if((!Number.isNaN(n1)) && (!Number.isNaN(n2))){
+    switch (s1) {
+        case "+": t1 = n1 + n2; break;
+        case "-": t1 = n1 - n2; break;
+        case "*": t1 = n1 * n2; break;
+        case "/": t1 = n1 / n2; break;
+        default: t1 = "An error has ocurred with the symbols";
+    }
+} else {
+    alert("An error has ocurred with the numbers");
 }
-if (win){
-    alert("Winner!");
-}
+alert(t1)
