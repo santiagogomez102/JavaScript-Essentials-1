0e5d8f3b0b7d6c527b496c699b57458532f0e1a2
santiagogomez102 <santiago.gomez102@outlook.com>
2024-01-24 16:18:03 -0500

    4.1.7
    for ... in

--- Cuarto Modulo.js
+++ Cuarto Modulo.js
@@ -1,31 +1,14 @@
 "use strict"
 
-let values = [10, 30, 50, 100];
-let sum = 0;
-for (let i = 0; i < values.length; i++) {
-    sum += values[i];
-}
-console.log(sum);
+let user = {
+    name: "Calvin",
+    surname: "Hart",
+    age: 66,
+    email: "CalvinMHart@teleworm.us"
+};
 
-sum = 0;
-for (let number of values) {
-    sum += number;
-}
-console.log(sum);
+console.log(user.name);
 
-let cities = [
-    { name: "New York", population: 18.65e6 },
-    { name: "Cairo", population: 18.82e6 },
-    { name: "Mumbai", population: 19.32e6 },
-    { name: "Sao Pablo", population: 20.88e6 },
-    { name: "Mexico City", population: 21.34e6 },
-    { name: "Shangai", population: 23.48e6 },
-    { name: "Delhi", population: 25.87e6 },
-    { name: "Tokyo", population: 37.26e6 },
-];
-
-for (let city of cities) {
-    if (city.population > 20e6 && city.population < 25e6) {
-        console.log(`${city.name} (${city.population})`);
-    }
+for (let key in user){
+    console.log(`${key} -> ${user[key]}`);
 }
