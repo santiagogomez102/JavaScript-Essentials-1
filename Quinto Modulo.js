52b13251194cb931767f00670088021fc5fea3ac
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-06 13:14:37 -0500

    5.1.3
    First-class members

--- Quinto Modulo.js
+++ Quinto Modulo.js
@@ -1,6 +1,29 @@
 "use strict"
 
-function factorial (n) {
-    return n > 1 ? n * factorial(n-1) : 1;
+function showMessage(message) {
+    console.log(`Message: ${message}`);
 }
-console.log(factorial(6));
+let sm = showMessage;
+
+sm("This works!");
+console.log(typeof sm);
+
+function doNothing() {
+    return undefined;
+}
+let a = doNothing();
+let b = doNothing;
+console.log(typeof a);
+console.log(typeof b);
+
+function add(a, b) {
+    return a + b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function operation(func, first, second) {
+    return func(first, second);
+}
+console.log(operation(add, 10, 20));
+console.log(operation(multiply, 10, 20));
