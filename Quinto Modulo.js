8537272711c762c7fde4bd4dfb97221e6e672eb4
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-06 12:33:15 -0500

    5.0.6
    Parameters

--- Quinto Modulo.js
+++ Quinto Modulo.js
@@ -1,16 +1,16 @@
 "use strict"
 
-let temperatures;
-
-function getMeanTemp() {
-    let sum = 0;
-    for (let i = 0; i < temperatures.length; i++){
-        sum += temperatures[i];
-    }
-    return sum / temperatures.length;
+function add(first, second) {
+    return first + second;
 }
 
-temperatures = [12, 12, 11, 11, 10, 9, 9, 10, 12, 13, 15, 18, 21, 24, 24, 23, 25, 25, 23, 21, 20, 19, 17, 16];
-console.log(`mean: ${getMeanTemp()}`);
-temperatures = [17, 16, 14, 12, 10, 10, 10, 11, 13, 14, 15, 17, 22, 27, 29, 29, 27, 26, 24, 21, 19, 18, 17, 16];
-console.log(`mean: ${getMeanTemp()}`);
+let result = add(5,7);
+console.log(result);
+
+function getElement(elements, index) {
+    return elements[index];
+}
+
+let names = ["Alice", "Bob", "Eve", "John"];
+let name = getElement(names, 2);
+console.log(name);
