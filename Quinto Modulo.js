ca6179ae5d0c4e5106f8a21ada1f39e4b7a0c12f
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-06 13:24:47 -0500

    5.1.4
    Function expressions

--- Quinto Modulo.js
+++ Quinto Modulo.js
@@ -1,29 +1,12 @@
 "use strict"
 
-function showMessage(message) {
-    console.log(`Message: ${message}`);
-}
-let sm = showMessage;
-
-sm("This works!");
-console.log(typeof sm);
-
-function doNothing() {
-    return undefined;
-}
-let a = doNothing();
-let b = doNothing;
-console.log(typeof a);
-console.log(typeof b);
-
-function add(a, b) {
-    return a + b;
-}
-function multiply(a, b){
-    return a * b;
-}
 function operation(func, first, second) {
     return func(first, second);
 }
-console.log(operation(add, 10, 20));
-console.log(operation(multiply, 10, 20));
+let myAdd = function(a, b) {
+    return a + b;
+}
+console.log(operation(myAdd, 10, 20));
+console.log(operation(function(a, b) {
+    return a * b;
+}, 10, 20));
