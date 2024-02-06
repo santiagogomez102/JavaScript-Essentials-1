a9fe4560842b9370256ee06110ad13d69c87861c
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-06 12:12:21 -0500

    5.0.4
    Local variables

--- Quinto Modulo.js
+++ Quinto Modulo.js
@@ -1,11 +1,10 @@
 "use strict"
 
 let temperatures;
-let sum;
 let meanTemp;
 
 function getMeanTemp() {
-    sum = 0;
+    let sum = 0;
     for (let i = 0; i < temperatures.length; i++){
         sum += temperatures[i];
     }
