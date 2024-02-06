e14f4069eef37bede77a70056c6993218c2f7d3b
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-06 10:13:12 -0500

    4.1.11
    Practice

--- Cuarto Modulo.js
+++ Cuarto Modulo.js
@@ -87,26 +87,30 @@ for (let key in vessel) {
 
 ////////////
 
-alert("Input 2 numbers:");
+while (true) {
+    let firstNumber = prompt("Enter first number");
+    let secondNumber = prompt("Enter second number");
+    let operand = prompt("Enter operand (+, -, *, /)");
+    let result;
 
-let num1 = Number(prompt("Number 1", 0));
-let num2 = Number(prompt("Number 2", 0));
-
-alert("Input a character from the following options: +, -, *, /");
-
-let s1 = prompt("Symbol 1", "+, -, *, /");
-
-let t1;
-
-if((!Number.isNaN(num1)) && (!Number.isNaN(num2))){
-    switch (s1) {
-        case "+": t1 = num1 + num2; break;
-        case "-": t1 = num1 - num2; break;
-        case "*": t1 = num1 * num2; break;
-        case "/": t1 = num1 / num2; break;
-        default: t1 = "An error has ocurred with the symbols";
+    if (firstNumber === "Q" || secondNumber === "Q" || operand === "Q") {
+        break;
     }
-} else {
-    alert("An error has ocurred with the numbers");
+
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (!Number.isNaN(firstNumber) && !Number.isNaN(secondNumber)) {
+        switch (operand) {
+            case "+": result = firstNumber + secondNumber; break;
+            case "+": result = firstNumber - secondNumber; break;
+            case "+": result = firstNumber * secondNumber; break;
+            case "+": result = firstNumber / secondNumber; break;
+            default: result = "Error: unknown operand";
+        }
+    } else {
+        result = "Error: at least one of the entered values is not a number";
+    }
+    
+    alert(result);
 }
-alert(t1)
