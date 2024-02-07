44f9dff7571b80b267c836362e3465c41aca6c07
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-06 23:55:50 -0500

    5.1.10
    Practice

--- Quinto Modulo.js
+++ Quinto Modulo.js
@@ -1,37 +1,110 @@
 "use strict"
 
-let add = function(a, b) {
-    return a + b;
+let numbers = [50, 10, 40, 30, 20];
+function compareNumbers(a, b) {
+    let retVal = 0;
+    if (a < b) {
+        retVal = -1;
+    } else if (a > b){
+        retVal = 1;
+    }
+    return retVal;
 }
-console.log(add(10, 20));
+let sorted = numbers.sort(compareNumbers);
+console.log(sorted);
 
-//////////
-
-add = (a, b) => {
-    return a + b;
-}
-console.log(add(10, 20));
+let numbers1 = [50, 10, 40, 30, 20];
+console.log(numbers1.sort((a, b) => a - b));
 
 ///////////
 
-add = (a, b) => a + b;
-console.log(add(10, 20));
+console.log(numbers1.sort((a, b) => b - a));
 
-function factorial(n) {
-    return n > 1 ? n * factorial(n - 1) : 1;
+////////////
+
+function add(a, b) {
+    return ((Number.isInteger(a)) && (Number.isInteger(b))) ? a + b : NaN;
 }
-console.log(factorial(5));
-
-factorial = n => n > 1 ? n * factorial(n - 1) : 1;
-console.log(factorial(5));
-
-//////////////
-
-let names = ['Alice', 'Eve', 'John'];
-function showName(element) {
-    console.log(element);
+function sub(a, b) {
+    return ((Number.isInteger(a)) && (Number.isInteger(b))) ? a - b : NaN;
 }
-names.forEach(showName);
+function mult(a, b) {
+    return ((Number.isInteger(a)) && (Number.isInteger(b))) ? a * b : NaN;
+}
+console.log(`Add: 2 + 3 = ${add(2, 3)}`);
+console.log(`Add: 2 + 3.2 = ${add(2, 3.2)}`);
+console.log(`Sub: 2 - 3 = ${sub(2, 3)}`);
+console.log(`Sub: 2 - 3.2 = ${sub(2, 3.2)}`);
+console.log(`Mult: 2 * 3 = ${mult(2, 3)}`);
+console.log(`Mult: 2 * 3.2 = ${mult(2, 3.2)}`);
 
-showName = (element) => console.log(element);
-names.forEach(showName);
+////////////
+
+let add1 = (a, b) => ((Number.isInteger(a)) && (Number.isInteger(b))) ? a + b : NaN;
+let sub1 = (a, b) => ((Number.isInteger(a)) && (Number.isInteger(b))) ? a - b : NaN;
+let mult1 = (a, b) => ((Number.isInteger(a)) && (Number.isInteger(b))) ? a * b : NaN;
+
+console.log(`Add: 2 + 3 = ${add1(2, 3)}`);
+console.log(`Add: 2 + 3.2 = ${add1(2, 3.2)}`);
+console.log(`Sub: 2 - 3 = ${sub1(2, 3)}`);
+console.log(`Sub: 2 - 3.2 = ${sub1(2, 3.2)}`);
+console.log(`Mult: 2 * 3 = ${mult1(2, 3)}`);
+console.log(`Mult: 2 * 3.2 = ${mult1(2, 3.2)}`);
+
+/////////////
+
+let add2 = (a, b) => ((Number.isInteger(a)) && (Number.isInteger(b))) ? a + b : NaN;
+let sub2 = (a, b) => ((Number.isInteger(a)) && (Number.isInteger(b))) ? a - b : NaN;
+let mult2 = (a, b) => ((Number.isInteger(a)) && (Number.isInteger(b))) ? a * b : NaN;
+function action(func, num1, num2){
+    return func(num1, num2);
+}
+console.log(`Add: 2 + 3 = ${action(add2, 2, 3)}`);
+console.log(`Add: 2 + 3.2 = ${action(add2, 2, 3.2)}`);
+console.log(`Sub: 2 - 3 = ${action(sub2, 2, 3)}`);
+console.log(`Sub: 2 - 3.2 = ${action(sub2, 2, 3.2)}`);
+console.log(`Mult: 2 * 3 = ${action(mult2, 2, 3)}`);
+console.log(`Mult: 2 * 3.2 = ${action(mult2, 2, 3.2)}`);
+
+//////////////////
+
+let counter = 1;
+let intervalId = setInterval(function () {
+    console.log(counter++);
+}, 2000);
+setTimeout(function() {
+    clearInterval(intervalId)
+}, 20000);
+
+///////////////
+
+let fibbRec = function(n) {
+    let retVal = 0;
+    if (n != 0) {
+        if (n === 1) {
+            retVal = 1;
+        } else {
+            retVal = fibbRec(n-1) + fibbRec(n - 2);
+        }
+    }
+    return retVal;
+}
+console.log(fibbRec(7));
+
+////////////////
+
+let fibb = n => n == 0 ? 0 : (n == 1 ? 1 : fibb(n - 1) + fibb(n - 2));
+console.log(fibb(8));
+
+///////////////
+
+function fibb1(n) {
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++){
+        let c = a;
+        a = b;
+        b += c;
+    }
+    return b;
+}
+console.log(fibb1(8));
