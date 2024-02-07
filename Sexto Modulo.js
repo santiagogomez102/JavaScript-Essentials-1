9beacb65d8486bc80f24b16b6e5a70ff4be0d01a
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-07 11:42:07 -0500

    6.1.6
    "throw" statement and custom errors

--- Sexto Modulo.js
+++ Sexto Modulo.js
@@ -1,14 +1,25 @@
 "use strict"
 
-let a = 10;
+console.log("start");
 try {
-    a = b;
-}  catch (error) {
-    try {
-        console.log(b);
-    } catch {
-        console.log("Second catch!");
-    }
-} finally {
-    console.log("Finally!")
+    throw 100;
+} catch (error) {
+    console.log(error);
 }
+console.log("end");
+
+function factorial(n) {
+    if (n > 20) {
+        throw new RangeError("Max value 20");
+    }
+    let result = 1;
+    for (; n > 1; n--) {
+        result = result * n;
+    }
+    return result;
+}
+console.log(factorial(3));
+console.log(factorial(5));
+console.log(factorial(8));
+console.log(factorial(20));
+console.log(factorial(1000));
