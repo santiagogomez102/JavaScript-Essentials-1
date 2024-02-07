7a44e1dccd32619be2b3d0d04aa51df8d4a6736e
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-07 01:36:43 -0500

    5.1.11
    LAB 2

--- LAB.js
+++ LAB.js
@@ -1,5 +1,43 @@
 "use strict"
 
+let showContact = function(list, index) {
+    if (list instanceof Array && list[index]) {
+        alert(`${list[index].name} / ${list[index].phone} / ${list[index].email}`);
+    }
+}
+let showAllContacts = function(list) {
+    if (list instanceof Array) {
+        for (let contact of list) {
+            alert(`${contact.name} / ${contact.phone} / ${contact.email}`);
+        }
+    }
+}
+let addNewContact = function(list, name, phone, email) {
+    if (list instanceof Array && name && phone && email) {
+        list.push({
+            name: name,
+            phone: phone,
+            email: email
+        })
+    }
+}
+let sortBy = function(list, type) {
+    if (list instanceof Array && type) {
+        switch (type) {
+            case "1":
+                list.sort((n1, n2) => (n1.name < n2.name) ? -1 : (n1.name > n2.name) ? 1 : 0);
+                break;
+            case "2":
+                list.sort((n1, n2) => (n1.phone < n2.phone) ? -1 : (n1.name > n2.phone) ? 1 : 0);
+                break;
+            case "3":
+                list.sort((n1, n2) => (n1.email < n2.email) ? -1 : (n1.email > n2.email) ? 1 : 0);
+                break;
+            default: alert("Error with option input");
+        }
+    }
+}
+
 let contacts = [
     {
         name: "Maxwell Wright",
@@ -22,8 +60,8 @@ while (true) {
 
     let last = contacts.length - 1;
 
-    let option = prompt("Select an action: 1. Show a contact 2. Show all contacts 3. Add new contact 4. Exit program", "1, 2, 3, 4");
-    if (option === "4"){
+    let option = prompt("Select an action: 1. Show a contact 2. Show all contacts 3. Add new contact 4. Sort contacts 5. Exit program", "1, 2, 3, 4, 5");
+    if (option === "5"){
         break;
     }
     switch (option) {
@@ -49,34 +87,10 @@ while (true) {
                 alert(`${contacts[last].name} / ${contacts[last].phone} / ${contacts[last].email}`);
             }
             break;
+        case "4":
+            let sortType = prompt("Sort by: 1. Name 2. Phone 3. Email", "1, 2, 3");
+            sortBy(contacts, sortType);
+            break;
         default: alert("Error with option input");
     }
-
-}
-
-function showContact(list, index) {
-    if(list instanceof Array) {
-        alert(`${list[index].name} / ${list[index].phone} / ${list[index].email}`);
-    } else {
-        alert(`Parameter error`);
-    }
-}
-
-function showAllContacts(list) {
-    if(list instanceof Array) {
-        for (let i = 0; i <= (list.length - 1) ; i++){
-            alert(`${list[i].name} / ${list[i].phone} / ${list[i].email}`);
-        }
-    } else {
-        alert(`Parameter error`);
-    }
-}
-
-function addNewContact(list, name, phone, email) {
-    let newContact = {
-        name: name,
-        phone: phone,
-        email: email
-    }
-    list.push(newContact);
 }
