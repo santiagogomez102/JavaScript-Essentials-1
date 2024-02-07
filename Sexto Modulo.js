83c0fb9a97bfdef2e1c8f6a5b3c97cfb22ec50f9
santiagogomez102 <santiago.gomez102@outlook.com>
2024-02-07 11:14:33 -0500

    6.1.3
    Conditional exception handling

--- Sexto Modulo.js
+++ Sexto Modulo.js
@@ -1,48 +1,14 @@
 "use strict"
 
+let a = -2;
 try {
-    let a = b;
-} catch (e) {
-    console.log("Caught " + e);
-}
-console.log("We handled the exception!");
-
-try {
-    let testArray1 = Array(10);
-    console.log(testArray1.length);
-    let testArray2 = Array(-1);
-    console.log(testArray1.length);
+    a = b;
 } catch (error) {
-    console.log("Caught " + error);
+    if (error instanceof ReferenceError) {
+        console.log("Reference error, reset a to -2");
+        a = -2;
+    } else {
+        console.log("Other error - " + error);
+    }
 }
-console.log("We handled the exception!");
-
-try {
-    let someNumber = 10;
-    someNumber.length();
-} catch (er) {
-    console.log("Caught " + er);
-}
-console.log("We handled the exception!");
-
-try {
-    const someConstValue = 5;
-    someConstValue = 7;
-} catch (err) {
-    console.log("Caught " + err);
-}
-console.log("We handled the exception!");
-
-try {
-    fun();
-} catch (erro) {
-    console.log("Caught " + erro);
-}
-console.log("We handled the exception!");
-
-try {
-    let a = b;
-} catch (errorVal) {
-    console.log("Caught " + errorVal);
-}
-console.log("We handled the exception!");
+console.log(a);
